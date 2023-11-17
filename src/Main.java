ec39c15fbcba56d3f4b48f7a18b1c27d69ffb227
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2023-11-17 17:20:15 -0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
+import entities.Circle;
 import entities.Product;
+import entities.Rectangle;
+import entities.Shape;
 import services.CalculationService;
 import services.PrintService;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.Buffer;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,11 +23,47 @@ public class Main {
 
         Uso comum: coleções
 
+        TIPOS CURINGA (wildcards)
+
+        Generics são invariantes
+        List<Object> não é o supertipo de qualquer tipo de lista
+
+        O supertipo de qualquer tipo de lista é List<?>. É um tipo curinga
+
+        Porém, não é possível adicionar dados a uma coleção de tipos curinga
+
+        TIPOS CURINGA DELIMITADOS
+
 
          */
 
 //        lerValores();
-        lerProdutos();
+//        lerProdutos();
+
+//        List<Integer> myInts = Arrays.asList(5,2,10);
+//        printList(myInts);
+//
+//        List<String> myStrs = Arrays.asList("Maria", "Alex", "Bob");
+//        printList(myStrs);
+
+        List<Shape> myShapes = new ArrayList<>();
+        myShapes.add(new Rectangle(3.0, 2.0));
+        myShapes.add(new Circle(2.0));
+
+        List<Circle> myCircles = new ArrayList<>();
+        myCircles.add(new Circle(2.0));
+        myCircles.add(new Circle(3.0));
+
+        System.out.println("total area: " + totalArea(myCircles));
+    }
+
+    // Essa declaração define que a lista pode ser de shape ou de qualquer tipo de shape
+    public static double totalArea(List<? extends Shape> list){
+        double sum = 0.0;
+        for (Shape s : list){
+            sum += s.area();
+        }
+        return sum;
     }
 
     public static void lerValores(){
@@ -71,6 +107,27 @@ public class Main {
 
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static void wildcards(){
+        List<?> myObjs = new ArrayList<Objects>();
+        List<Integer> myNumbers = new ArrayList<Integer>();
+        myObjs = myNumbers;
+
+        // List<?> é o supertipo de qualquer tipo de lista
+
+        Object obj;
+        Integer x = 10;
+        obj = x;
+        // assim funciona
+
+    }
+
+    public static void printList(List<?> list) {
+        // método para imprimir qualquer tipo de lista
+        for (Object obj : list){
+            System.out.println(obj);
         }
     }
 }
